44a1fc082bc8ee31ba676ddac987afd28b292bc9
BRIKSI Reda <briksireda@gmail.com>
2020-12-29 15:18:11 +0100

    initial commit

---
+++ WcfService_Enseignement/WcfService_Enseignement/IServiceEnseignement.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace WcfService_Enseignement
+{
+    // REMARQUE : vous pouvez utiliser la commande Renommer du menu Refactoriser pour changer le nom d'interface "IServiceEnseignement" à la fois dans le code et le fichier de configuration.
+    [ServiceContract]
+    public interface IServiceEnseignement
+    {
+        [OperationContract]
+        void DoWork();
+
+        [OperationContract]
+        List<Eleves> getByCNE(string s);
+    }
+}
---
+++ WcfService_Enseignement/WcfService_Enseignement/ServiceEnseignement.svc.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace WcfService_Enseignement
+{
+    // REMARQUE : vous pouvez utiliser la commande Renommer du menu Refactoriser pour changer le nom de classe "ServiceEnseignement" à la fois dans le code, le fichier svc et le fichier de configuration.
+    // REMARQUE : pour lancer le client test WCF afin de tester ce service, sélectionnez ServiceEnseignement.svc ou ServiceEnseignement.svc.cs dans l'Explorateur de solutions et démarrez le débogage.
+    public class ServiceEnseignement : IServiceEnseignement
+    {
+        public void DoWork()
+        {
+        }
+
+        DataClassesEnseignementDataContext l = new DataClassesEnseignementDataContext();
+        public List<Eleves> getByCNE(string s)
+        {
+            return (from p in l.Eleves where p.CNE == s select p).ToList();
+        }
+
+
+    }
+}
